76d7b6ac7b5eb2349fbd8347a8bf8034f064ea27
Robin Verlangen <robin@us2.nl>
2015-05-05 19:09:53 +0200

    Ignore src

--- server/build.sh
+++ server/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export GOPATH=`pwd`
+go get -t -v ./...
 go build .
--- server/server.go
+++ server/server.go
@@ -4,9 +4,24 @@
 package main
 
 import (
+	"golang.org/x/net/websocket"
+	"io"
 	"log"
+	"net/http"
 )
 
+// Echo the data received on the WebSocket.
+func EchoServer(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
+// This example demonstrates a trivial echo server.
 func main() {
 	log.Println("Starting")
+	http.Handle("/echo", websocket.Handler(EchoServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+	log.Println("Shutting down")
 }
